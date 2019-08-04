19a442ab7e8ca5e38cff48045ef5db24e5c48032
Lichen Ma <lichenma123@gmail.com>
2019-08-04 02:03:20 -0400

    update

--- app/src/main/java/com/example/timerapplication/util/NotificationUtil.kt
+++ app/src/main/java/com/example/timerapplication/util/NotificationUtil.kt
@@ -1,15 +1,21 @@
 package com.example.timerapplication.util
 
+import android.annotation.TargetApi
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.timerapplication.MainActivity
 import com.example.timerapplication.R
 import com.example.timerapplication.TimerNotificationActionReceiver
+import java.text.SimpleDateFormat
 
 class NotificationUtil {
     // creating the companion object allows us to call this function
@@ -35,7 +41,39 @@ class NotificationUtil {
                 // calls the pending intent when clicked
                 .addAction(R.drawable.ic_play, "Start", startPendingIntent)
 
-            val nManager = 
+            val nManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            nManager.createNotificationChannel(CHANNEL_ID_TIMER, CHANNEL_NAME_TIMER, true)
+            nManager.notify(TIMER_ID, nBuilder.build())
+        }
+
+        fun showTimerRunning(context: Context, wakeUpTime: Long){
+            // we want to be control timer from notification
+            // allow starting the timer from notification
+            val stopIntent = Intent(context, TimerNotificationActionReceiver::class.java)
+            // string
+            stopIntent.action = AppConstants.ACTION_STOP
+            val stopPendingIntent = PendingIntent.getBroadcast(context, 0, stopIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val pauseIntent = Intent(context, TimerNotificationActionReceiver::class.java)
+            // string
+            pauseIntent.action = AppConstants.ACTION_START
+            val pausePendingIntent = PendingIntent.getBroadcast(context, 0, pauseIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val df = SimpleDateFormat.getTimeInstance(SimpleDateFormat.SHORT)
+            
+
+
+
+            val nBuilder = getBasicNotificationBuilder(context, CHANNEL_ID_TIMER, true)
+            nBuilder.setContentTitle("Timer Expired!")
+                .setContentText("Start again?")
+                .setContentIntent(getPendingIntentWithStack(context, MainActivity::class.java))
+                // calls the pending intent when clicked
+                .addAction(R.drawable.ic_play, "Start", startPendingIntent)
+
+            val nManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            nManager.createNotificationChannel(CHANNEL_ID_TIMER, CHANNEL_NAME_TIMER, true)
+            nManager.notify(TIMER_ID, nBuilder.build())
         }
 
         private fun getBasicNotificationBuilder(context: Context, channelId: String, playSound: Boolean): NotificationCompat.Builder {
@@ -65,5 +103,21 @@ class NotificationUtil {
 
             return stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
         }
+
+        // extension function which extends a class by one function
+        // allows us to edit the code of the class - in this case the class
+        // notification manager
+        @TargetApi(26)
+        private fun NotificationManager.createNotificationChannel(channelId: String, channelName: String, playSound: Boolean){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                val channelImportance = if (playSound) NotificationManager.IMPORTANCE_DEFAULT
+                else NotificationManager.IMPORTANCE_LOW
+                val nChannel = NotificationChannel(channelId, channelName, channelImportance)
+                nChannel.enableLights(true)
+                nChannel.lightColor = Color.BLUE
+                this.createNotificationChannel(nChannel)
+            }
+        }
+
     }
 }
